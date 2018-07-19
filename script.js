20a050ee61828a81d73803ddc5325561143f9315
sanchay-kothari <sanchay.v.k@gmail.com>
2018-07-19 20:05:15 +0530

    upload2

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var h3 = document.querySelector("h3");
 var input1 = document.getElementById("inp1");
 var input2 = document.getElementById("inp2");
 var bg = document.getElementById("getcolor");
-var primarycolor = bg.getAttribute("style");
+var primarycolor = bg.style.backgroundImage;
 h3.textContent = primarycolor + ";";
 function colorPicker(){
-	bg.style.background = "linear-gradient(to right, "+ input1.value +" , " + input2.value +")";
-	h3.textContent = bg.style.background + ";";
+	bg.style.backgroundImage = "linear-gradient(to right, "+ input1.value +" , " + input2.value +")";
+	h3.textContent = bg.style.backgroundImage + ";";
 }
 input1.addEventListener("input",colorPicker);
 input2.addEventListener("input",colorPicker);
